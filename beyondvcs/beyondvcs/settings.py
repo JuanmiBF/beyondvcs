bbd4cc0ef1854b8fd03a9cb8dd3687743b1d7f9f
Juanmi <juanmiblancof@gmail.com>
2019-02-24 20:59:50 +0100

    Added heroku files

--- beyondvcs/beyondvcs/settings.py
+++ beyondvcs/beyondvcs/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'commitwaitlist.apps.CommitwaitlistConfig'
+    'commitwaitlist.apps.CommitwaitlistConfig',
+]
+
+MODULES = [
+    'commitwaitlist',
 ]
 
 MIDDLEWARE = [
