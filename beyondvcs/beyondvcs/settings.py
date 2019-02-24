f832b268340af80417b15cfee8c409a4c425090d
Juanmi <juanmiblancof@gmail.com>
2019-02-24 19:35:03 +0100

    Added heroku files

--- beyondvcs/beyondvcs/settings.py
+++ beyondvcs/beyondvcs/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'commitwaitlist.apps.CommitwaitlistConfig'
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'beyondvcs.beyondvcs.urls'
+ROOT_URLCONF = 'beyondvcs.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'beyondvcs.beyondvcs.wsgi.application'
+WSGI_APPLICATION = 'beyondvcs.wsgi.application'
 
 
 # Database
